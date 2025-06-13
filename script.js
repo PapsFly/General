cdba71968dc16ca82b2bdb6a9f6bf9d26b2e4d2d
Draker <82230448+PapsFly@users.noreply.github.com>
2025-06-13 18:23:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function openCategory(categoryId) {
+  console.log("Ouverture de la catégorie :", categoryId); // DEBUG
   const popup = document.getElementById(categoryId + "-popup");
   if (popup) {
     popup.classList.remove("hidden");
   }
 }
+
 
 function closePopup(id) {
   const popup = document.getElementById(id);
